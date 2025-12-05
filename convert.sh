94a622e4ce0c5c8b41f4ee5f5c2497103d5b85d0
EduardPrigoana <eduard@prigoana.com>
2025-12-05 19:56:58 +0200

    5000 bands

--- convert.sh
+++ convert.sh
@@ -3,9 +3,16 @@ set -euo pipefail
 
 mkdir -p webp
 
-find png -maxdepth 1 -type f -name "*.png" -print0 \
+find png -maxdepth 1 -type f \( \
+    -iname "*.png" -o \
+    -iname "*.jpg" -o \
+    -iname "*.jpeg" -o \
+    -iname "*.webp" -o \
+    -iname "*.avif" \
+\) -print0 \
 | xargs -0 -P "$(nproc)" -I{} bash -c '
 in="$1"
-b="$(basename "$in" .png)"
-cwebp -q 50 -m 6 -mt -metadata none -resize 500 500 "$in" -o "webp/$b.webp"
+name="$(basename "$in")"
+base="${name%.*}"
+cwebp -q 50 -m 6 -mt -metadata none -resize 500 500 "$in" -o "webp/$base.webp"
 ' _ "{}"
