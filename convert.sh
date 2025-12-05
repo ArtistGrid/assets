750f8ef85d825cc1fb416e9e55b0d2b3cbd2c92b
EduardPrigoana <eduard@prigoana.com>
2025-12-05 15:13:50 +0200

    c

--- convert.sh
+++ convert.sh
@@ -3,12 +3,9 @@ set -euo pipefail
 
 mkdir -p webp
 
-export -f convert_one
-convert_one() {
-    in="$1"
-    b="$(basename "$in" .png)"
-    cwebp -q 50 -m 6 -mt -metadata none -resize 500 500 "$in" -o "webp/$b.webp"
-}
-
-find png -maxdepth 1 -type f -name "*.png" -print0 |
-xargs -0 -P"$(nproc)" -I{} bash -c 'convert_one "$@"' _ "{}"
+find png -maxdepth 1 -type f -name "*.png" -print0 \
+| xargs -0 -P "$(nproc)" -I{} bash -c '
+in="$1"
+b="$(basename "$in" .png)"
+cwebp -q 50 -m 6 -mt -metadata none -resize 500 500 "$in" -o "webp/$b.webp"
+' _ "{}"
